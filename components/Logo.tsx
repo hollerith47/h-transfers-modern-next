c1bf0a0d5c93439d9f773802bf6ba8f8118c7878
Hmak47 <hermanhmakiese@gmail.com>
2025-05-04 01:27:36 +0300

    modification logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
-import logoImg from "../public/logo.png"
+import logoImg from "../public/logo.png";
 
 export default function Logo() {
     return (
         <Link href="/"  className="flex font-bold items-center">
-            <Image src={logoImg} alt={"Logo H Tech"} width={"50"} height={"50"} />
+            {/*<Image src={logoImg} alt={"Logo H Tech"} width={"50"} height={"50"} />*/}
+            <span className="text-2xl md:text-3xl bg-primary text-white">H</span>
             <span className="text-2xl md:text-3xl text-primary">Transfers</span>
         </Link>
 
