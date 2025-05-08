bb94f1d47e79f4ebe947f2659f8b67a1a21c4447
Hmak47 <hermanhmakiese@gmail.com>
2025-05-09 00:50:39 +0300

    dashboard almost working some visibility bug fixed

--- app/(auth)/DashboardContainer.tsx
+++ app/(auth)/DashboardContainer.tsx
@@ -5,7 +5,7 @@ import EvolutionChartContainer from "@/app/(auth)/EvolutionChartContainer";
 export default function DashboardContainer() {
 
     return (
-        <div className="p-6 space-y-8">
+        <div className="space-y-8">
             <SoldContainer />
             <EvolutionChartContainer />
         </div>
